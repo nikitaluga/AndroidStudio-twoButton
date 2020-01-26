acb157c0c9604a64fa8e9d87e35e1e73837376d1
lugovoyNikita <zenit27069621@mail.ru>
2020-01-26 11:52:26 +0300

    add functionality of the second button

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.myapplication1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewTop;
+    TextView textViewBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTop = findViewById(R.id.labelTop);
+        textViewBottom = findViewById(R.id.labelBottom);
     }
-    public void onClose(View view){
-        this.finish();
+
+    public void onChangeTextViewTop(View view) {
+        textViewTop.setText("Hi SkillBox");
+    }
+
+    public void onChangeTextTextViewBottom(View view) {
+        textViewBottom.setText("Эта кнопка работает !");
     }
 }
